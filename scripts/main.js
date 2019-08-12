7fb6eca86f6329b3fdc0f30a58f22d1cd61be3fc
Eythan Saillet <eythansaillet@Leo-Lange.local>
2019-08-12 14:22:16 +0200

    syle : fonts, colors and strokes

--- scripts/main.js
+++ scripts/main.js
@@ -7,17 +7,18 @@ ceramic = document.querySelector('.ceramic')
 let trigger = true
 let staggerTranslateX = 0
 let staggerTranslateY = 0
+// let hey = new TimelineMax()
 ceramic.addEventListener('mouseover',function(){
     console.log(event.movementY,event.movementX)
     if(trigger == true){
         trigger = false
         setTimeout(function(){
             trigger = true
-        },700)
+        },00)
         TweenMax.staggerTo(ceramicTab, 0.5, {
             y: event.movementY*3,
             x: event.movementX*3,
-            opacity: 0.5,
+            color: "#FFFFFF",
             ease: Power1.easeInOut,
             stagger: {
                 from: ceramicMid,
@@ -29,7 +30,7 @@ ceramic.addEventListener('mouseover',function(){
                 TweenMax.staggerTo(ceramicTab, 0.5, {
                     y: 0,
                     x: 0,
-                    opacity: 1,
+                    color: "#000000",
                     ease: Power1.easeInOut,
                     stagger: {
                         from: ceramicMid,
