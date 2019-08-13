eb9a0db3270f16c18f2e32cce589b75029734b06
Eythan <eythan.saillet@gmail.com>
2019-08-13 16:34:09 +0200

    custom cursor + word display non outside of the screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,44 @@
-word = document.querySelector('.word')
-wordTab = document.querySelectorAll('.word span')
-let tab = []
-let wordTrigger = true
-let spanTrigger = true
-let letter
-let staggerFrom
-wordTab.forEach(function(e) {
+function wordSmash(){
+  let word = document.querySelector('.word')
+  let wordTab = document.querySelectorAll('.word span')
+  let tab = []
+  let wordTrigger = true
+  let spanTrigger = true
+  let hoverTrigger = true
+  let goBack = true
+
+  let letter
+  let staggerFrom
+  wordTab.forEach(function(e) {
     tab.push(e.innerHTML)
     e.addEventListener('mouseover',function(){
-        if(spanTrigger == true){
+        if(spanTrigger == true && hoverTrigger == true){
             spanTrigger = false
             letter = e.innerHTML
             staggerFrom = tab.indexOf(letter)
-            console.log(staggerFrom)
             setTimeout(function(){
                 spanTrigger = true
             },1000)
         }
     })
   })
-word.addEventListener('mouseover',function(){
-    if(wordTrigger == true){
+  word.addEventListener('mouseover',function(){
+    if(Math.abs(event.movementY*4)>screen.height/1.6){
+      console.log("oui")
+      goBack = false
+    }
+    let cursor = document.querySelector('.cursor')
+    if(wordTrigger == true && hoverTrigger == true){
         wordTrigger = false
+        TweenMax.to(cursor,0,{opacity:0})
         setTimeout(function(){
             wordTrigger = true
+            TweenMax.to(cursor,1,{opacity:1})
         },1000)
-        
+
         TweenMax.staggerTo(wordTab, 0.5, {
-            y: event.movementY*3.5,
-            x: event.movementX*3.5,
+            y: event.movementY*4,
+            x: event.movementX*4,
             color: "#FFFFFF",
             ease: Power1.easeInOut,
             stagger: {
@@ -37,19 +47,36 @@ word.addEventListener('mouseover',function(){
             }
         });
         setTimeout(
-            function(){
-                TweenMax.staggerTo(wordTab, 0.5, {
-                    y: 0,
-                    x: 0,
-                    color: "#000000",
-                    ease: Power1.easeInOut,
-                    stagger: {
-                        from: staggerFrom,
-                        amount: 0.4
-                    }
-                });
+          function(){
+            if (goBack == true) {
+              TweenMax.staggerTo(wordTab, 0.5, {
+                  y: 0,
+                  x: 0,
+                  color: "#000000",
+                  ease: Power1.easeInOut,
+                  stagger: {
+                      from: staggerFrom,
+                      amount: 0.4
+                  }
+              })
             }
-            ,500
-        )
-    } 
-})
+            else{
+              setTimeout(function(){
+                word.style.display = "none"
+              },200)
+            }
+          }
+        ,500)
+      }
+  })
+}
+wordSmash()
+
+function customCursor(){
+  let cursor = document.querySelector('.cursor')
+  window.addEventListener('mousemove', function(){
+    cursor.style.left = event.clientX+"px"
+    cursor.style.top = event.clientY+"px"
+  })
+}
+customCursor()
